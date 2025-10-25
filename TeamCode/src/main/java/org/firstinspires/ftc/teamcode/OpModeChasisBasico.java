2652d6ba495cb17037f752b20cb9b3959b65e4ba
GaboTx34 <alonsonavarretegabrielaxel34@gmail.com>
2025-10-24 23:05:33 -0600

    Archivo Chasis
    Agregue el archivo de la estructura de un programa basico del robot para agregar el chasis

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModeChasisBasico.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name="Chasis Prueba 1", group="RobotInicial")
+@Disabled
+public class OpModeChasisBasico extends LinearOpMode {
+
+    //Declarar variables//
+
+    @Override
+    public void runOpMode() {
+
+        waitForStart();
+
+        while (opModeIsActive()){
+
+            //Chamba del robot//
+
+
+        }
+
+    }
+}
